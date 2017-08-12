9a8a5fd11ad3c27000a987d0112e2089732122a6
bdbrown <bdbrown4@eagles.usi.edu>
2017-08-12 13:27:35 -0400

    linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauthapp',
+    database: 'mongodb://bdbrown4:Luismacias123@ds159208.mlab.com:59208/meanauthapp',
     secret: 'Bdbrown4$'
 }
